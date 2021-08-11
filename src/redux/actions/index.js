cdd948fb6882114aea9a3c46a795f42e6de95010
Eduardo Salinas <eduardo.salinas1@outlook.com>
2021-08-11 07:39:18 -0300

    update

--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -1,17 +1,12 @@
 import axios from "axios";
-require('dotenv').config()
 
 export const ADD_MOVIE_FAVORITE = "ADD_MOVIE_FAVORITE";
 export const REMOVE_MOVIE_FAVORITE = "REMOVE_MOVIE_FAVORITE";
 export const GET_MOVIES_DETAIL = "GET_MOVIES_DETAIL";
 export const GET_MOVIES = "GET_MOVIES";
 export const LOADING = "LOADING";
-const URL="http://www.omdbapi.com/?apikey=";
 
-const API_KEY = process.env.REACT_APP_API_KEY;
-
-
-
+const URL = process.env.REACT_APP_API_URL;
 
 export const addMovieFavorite = (payload) => {
     return { type: ADD_MOVIE_FAVORITE, payload };
@@ -21,12 +16,11 @@ export const removeMovieFavorite = (id) => {
     return { type: REMOVE_MOVIE_FAVORITE, payload: id };
 }
 
-export const getMovies = (title, page) => { //busco peliculas por titulo
+export const getMovies = (title, page) => { 
     return async (dispatch) => {
         dispatch({ type: LOADING });
-        console.log ("apii----------", URL)
         try {
-            const movies = await axios.get(`${URL+API_KEY}&s=${title}&page=${page}`)
+            const movies = await axios.get(`${URL}&s=${title}&page=${page}`)
             return movies.data.Response === "False" ?
                 dispatch({ type: GET_MOVIES, payload: { Search: [{ err: "Movie not found" }] } }) :
                 dispatch({ type: GET_MOVIES, payload: movies.data });
@@ -38,11 +32,11 @@ export const getMovies = (title, page) => { //busco peliculas por titulo
     };
 }
 
-export const getMovieDetail = (id) => { //busco pelicula por id y me trae sus detalles
+export const getMovieDetail = (id) => { 
     return async (dispatch) => {
         dispatch({ type: LOADING });
         try {
-            const movie = await axios.get(`${URL+API_KEY}&i=` + id)
+            const movie = await axios.get(`${URL}&i=` + id)
             return movie.data.Response === "False" ?
                 dispatch({ type: GET_MOVIES_DETAIL, payload: { err: "Movie not found" } }) :
                 dispatch({ type: GET_MOVIES_DETAIL, payload: movie.data });
